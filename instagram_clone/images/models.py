242dd2816e3f873b96b07f2e47e7ab5950e86c16
eugene94 <eugenekang94@gmail.com>
2018-06-27 15:27:12 +0900

    modify models(image, comment, like)

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from instagram_clone.users import models as user_model
 
-# Create your models here.
+
+class TimeStampModel (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampModel):
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+
+
+class Comment(TimeStampModel):
+    message = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+
+class Like(TimeStampModel):
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
