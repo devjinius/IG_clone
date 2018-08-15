43ba4f2989de1e492c9f45d897d2c50faeeeb80b
eugene94 <eugenekang94@gmail.com>
2018-08-15 15:29:03 +0900

    add jwt

--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
@@ -23,6 +18,7 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     path("images/", include("instagram_clone.images.urls", namespace="images")),
     path("notifications/", include("instagram_clone.notifications.urls", namespace="notifications")),
+    path("api-token-auth/", obtain_jwt_token),
 
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
